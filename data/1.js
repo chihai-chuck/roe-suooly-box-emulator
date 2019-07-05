eaa7448265f28ea2dc6f656c1d6cc9dbf3f24868
迟海 <chuck@chihai.me>
2019-07-05 15:01:31 +0800

    test

---
+++ data/1.js
@@ -0,0 +1,102 @@
+module.exports = [
+    {
+        name: "AK-15 – 烈焰",
+        rate: .75,
+        img: ""
+    },
+    {
+        name: "LVOA-C – 烈焰",
+        rate: .75,
+        img: ""
+    },
+    {
+        name: "莫辛-纳甘 – 领头虎",
+        rate: 2.5,
+        img: ""
+    },
+    {
+        name: "AK-47 – 领头虎",
+        rate: 2.5,
+        img: ""
+    },
+    {
+        name: "GROZA – 领头虎",
+        rate: 2.5,
+        img: ""
+    },
+    {
+        name: "MPX-SD – 领头虎",
+        rate: 2.5,
+        img: ""
+    },
+    {
+        name: "G28 – 编织者",
+        rate: 2.5,
+        img: ""
+    },
+    {
+        name: "M200 – 编织者",
+        rate: 2.5,
+        img: ""
+    },
+    {
+        name: "LVOA-C – 编织者",
+        rate: 2.5,
+        img: ""
+    },
+    {
+        name: "MP5 – 编织者",
+        rate: 2.5,
+        img: ""
+    },
+    {
+        name: "CITORI – 白昼",
+        rate: 3.08,
+        img: ""
+    },
+    {
+        name: "MPX-SD – 白昼",
+        rate: 3.25,
+        img: ""
+    },
+    {
+        name: "莫辛-纳甘 – 白昼",
+        rate: 3.42,
+        img: ""
+    },
+    {
+        name: "PKM – 白昼",
+        rate: 3.59,
+        img: ""
+    },
+    {
+        name: "LVOA-C – 海洋",
+        rate: 3.76,
+        img: ""
+    },
+    {
+        name: "AK-15 – 海洋",
+        rate: 3.93,
+        img: ""
+    },
+    {
+        name: "MP5 – 海洋",
+        rate: 4.1,
+        img: ""
+    },
+    {
+        name: "FAMAS – 海洋",
+        rate: 4.27,
+        img: ""
+    },
+    {
+        name: "VECTOR – 翡翠",
+        rate: 4.44,
+        img: ""
+    },
+    {
+        name: "M4A1 – 翡翠",
+        rate: 4.61,
+        img: ""
+    }
+];
---
+++ test.js
@@ -0,0 +1,3 @@
+const data = require("./data/1.js");
+
+console.log(data)
