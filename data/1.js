4092313e0d1d3add6d068df372ae60c681ee0c2c
迟海 <chuck@chihai.me>
2019-07-10 20:33:23 +0800

    实现开箱随机算法

--- data/1.js
+++ data/1.js
@@ -1,102 +1,102 @@
 module.exports = [
     {
         name: "AK-15 – 烈焰",
-        rate: .75,
+        rate: .0075,
         img: ""
     },
     {
         name: "LVOA-C – 烈焰",
-        rate: .75,
+        rate: .0075,
         img: ""
     },
     {
         name: "莫辛-纳甘 – 领头虎",
-        rate: 2.5,
+        rate: .025,
         img: ""
     },
     {
         name: "AK-47 – 领头虎",
-        rate: 2.5,
+        rate: .025,
         img: ""
     },
     {
         name: "GROZA – 领头虎",
-        rate: 2.5,
+        rate: .025,
         img: ""
     },
     {
         name: "MPX-SD – 领头虎",
-        rate: 2.5,
+        rate: .025,
         img: ""
     },
     {
         name: "G28 – 编织者",
-        rate: 2.5,
+        rate: .025,
         img: ""
     },
     {
         name: "M200 – 编织者",
-        rate: 2.5,
+        rate: .025,
         img: ""
     },
     {
         name: "LVOA-C – 编织者",
-        rate: 2.5,
+        rate: .025,
         img: ""
     },
     {
         name: "MP5 – 编织者",
-        rate: 2.5,
+        rate: .025,
         img: ""
     },
     {
         name: "CITORI – 白昼",
-        rate: 3.08,
+        rate: .0308,
         img: ""
     },
     {
         name: "MPX-SD – 白昼",
-        rate: 3.25,
+        rate: .0325,
         img: ""
     },
     {
         name: "莫辛-纳甘 – 白昼",
-        rate: 3.42,
+        rate: .0342,
         img: ""
     },
     {
         name: "PKM – 白昼",
-        rate: 3.59,
+        rate: .0359,
         img: ""
     },
     {
         name: "LVOA-C – 海洋",
-        rate: 3.76,
+        rate: .0376,
         img: ""
     },
     {
         name: "AK-15 – 海洋",
-        rate: 3.93,
+        rate: .0393,
         img: ""
     },
     {
         name: "MP5 – 海洋",
-        rate: 4.1,
+        rate: .041,
         img: ""
     },
     {
         name: "FAMAS – 海洋",
-        rate: 4.27,
+        rate: .0427,
         img: ""
     },
     {
         name: "VECTOR – 翡翠",
-        rate: 4.44,
+        rate: .0444,
         img: ""
     },
     {
         name: "M4A1 – 翡翠",
-        rate: 4.61,
+        rate: .0461,
         img: ""
     }
 ];
--- test.js
+++ test.js
@@ -1,3 +1,58 @@
 const data = require("./data/1.js");
 
-console.log(data)
+
+class SupplyBoxLottery {
+    constructor(list, key) {
+        this.list = list.sort((a, b) => b[key] - a[key]);
+        this.key = key;
+        this.prizeGroup = {};
+        this.setPrizeGroup();
+    }
+
+    setPrizeGroup() {
+        for(let item of this.list) {
+            if(this.prizeGroup[item[this.key]]) {
+                this.prizeGroup[item[this.key]].push(item);
+            } else {
+                this.prizeGroup[item[this.key]] = [item];
+            }
+        }
+    }
+
+    getPrizeGroup(key) {
+        const length = this.prizeGroup[key].length;
+        let index = 0;
+        if(length > 1) {
+            index = Math.floor(Math.random() * length);
+        }
+        return this.prizeGroup[key][index];
+    }
+
+    calcRandomPrize() {
+        let result = this.list[0][this.key];
+        let sum = this.list.reduce((a, b) => a + b[this.key], 0);
+        for(let item of this.list) {
+            const rand = Math.random() * sum;
+            const rate = item[this.key];
+            if(rand <= rate) {
+                result = rate;
+                break;
+            } else {
+                sum -= rate;
+            }
+        }
+        return this.getPrizeGroup(result);
+    }
+
+    getResult(num = 1) {
+        const res = [];
+        for(let i=0; i<num; i++) {
+            res.push(this.calcRandomPrize());
+        }
+        return res.length > 1 ? res : res[0];
+    }
+}
+
+
+const testC = new SupplyBoxLottery(data, "rate");
+console.log(testC.getResult(10));
